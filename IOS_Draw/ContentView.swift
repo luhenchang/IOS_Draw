ccb503b9e08b69e7d180025710d1eeb63ea68d81
luhenchang <1276998208@qq.com>
2020-12-09 13:21:59 +0800

    first commit

---
+++ IOS_Draw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_Draw
+//
+//  Created by 王飞 on 2020/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
